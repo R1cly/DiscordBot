dd0ab243e4fc4d8b3eabdd7a92d4f0f448536762
Ricardo Sevilla Climent <57192844+R1cly@users.noreply.github.com>
2021-01-19 09:20:30 +0100

    Ahora le bot se conecta al Discord

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("HelloFriend!ThisIsDiscordBot01")
+# botInfo
+#Importamos las librerias necesarias.
+import discord
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = 'ODAwOTk1Mzc2ODAyODI0MjAy.YAaPEw.gE-QTSHrRxFgm1HppHiNC7rB2ds'
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
